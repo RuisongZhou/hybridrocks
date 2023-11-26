dc0c1891bf6151011b17389356aada314ce95031
ruisongzhou <811437508@qq.com>
2023-11-26 22:00:30 +0800

    fix test

--- scripts/test_hybrid.sh
+++ scripts/test_hybrid.sh
@@ -29,12 +29,12 @@ sleep 5s
 # --stats_interval_seconds=5 --report_file="$output_name/fillrandom_report.txt" | tee $output_name/fillrandom_runresult.txt
 # sleep 5s
 
-# sync
-# sudo bash -c "echo 3 > /proc/sys/vm/drop_caches" 
-# ../db_bench --db=$dbname --db_path="$ssd_path:$ssd_size,$hdd_path:$hdd_size" --num=$num --use_existing_db=1  --compression_type=none --compression_ratio=1 \
-# --benchmarks="overwrite,stats" --statistics --value_size=$value_size  --max_bytes_for_level_base=$level0_size --target_file_size_base=$sst_size --report_interval_seconds=1 \
-# --stats_interval_seconds=5 --report_file="$output_name/overwrite_report.txt" | tee $output_name/overwrite_runresult.txt
-# sleep 5s
+sync
+sudo bash -c "echo 3 > /proc/sys/vm/drop_caches" 
+../db_bench --db=$dbname --db_path="$ssd_path:$ssd_size,$hdd_path:$hdd_size" --num=$num --use_existing_db=1  --compression_type=none --compression_ratio=1 \
+--benchmarks="overwrite,stats" --statistics --value_size=$value_size  --max_bytes_for_level_base=$level0_size --target_file_size_base=$sst_size --report_interval_seconds=1 \
+--stats_interval_seconds=5 --report_file="$output_name/overwrite_report.txt" | tee $output_name/overwrite_runresult.txt
+sleep 5s
 
 # sync
 # sudo bash -c "echo 3 > /proc/sys/vm/drop_caches" 
